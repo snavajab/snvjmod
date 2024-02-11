f8b548fac05283a0f1c115f0e462b528fd30ca09
Tommaso Morganti <tommaso.morganti@mail.polimi.it>
2024-02-11 04:42:35 +0100

    init

---
+++ src/client/java/snvj/snvjmod/SnvjmodClient.java
@@ -0,0 +1,10 @@
+package snvj.snvjmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SnvjmodClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
