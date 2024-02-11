025d4a440ce7cff532737b18bfe814b1cb4e0f82
Tommaso Morganti <tommaso.morganti@mail.polimi.it>
2024-02-11 05:35:17 +0100

    add a single if statement

--- src/main/java/snvj/snvjmod/Snvjmod.java
+++ src/main/java/snvj/snvjmod/Snvjmod.java
@@ -1,9 +1,14 @@
 package snvj.snvjmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static net.minecraft.server.command.CommandManager.*;
 
 public class Snvjmod implements ModInitializer {
     // This logger is used to write text to the console and the log file.
@@ -16,6 +21,21 @@ public class Snvjmod implements ModInitializer {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, env) -> {
+            dispatcher.register(literal("snvj").executes(context -> {
+                ItemStack stack = context.getSource().getPlayer().getMainHandStack();
+
+                if (stack.getItem().toString() == "map") {
+                    context.getSource().sendFeedback(
+                            () -> Text.literal("Selected slot: " + (stack.getItem().toString() == "map")),
+                            false);
+                    return 1;
+                }
+                return 1;
+            }));
+        });
+
         LOGGER.info("Hello Snvj world!");
     }
 }
